90cdf85d3b12ec56aad95bb51882bd5b68c5e37b
Mohammed Fayaz z <mohammedfayazfn173148@gmail.com>
2025-08-06 16:34:54 +0530

    send message done

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 // You can add interactivity here if needed
 console.log("Page Loaded");
+document
+  .querySelector(".contact-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Your message has been submitted!");
+  });
